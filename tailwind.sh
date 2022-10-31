70f7795878a95914da889e251e227c3389c85084
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-10-31 22:38:52 +0100

    Add container screens to generated tailwind config

--- tailwind.sh
+++ tailwind.sh
@@ -33,6 +33,12 @@ echo -e $tailwindInit > input.css
 tailwindConfig='module.exports = {
   content: ["./*.html"],
   theme: {
+     screens: {
+      sm: "480px",
+      md: "768px",
+      mg: "976px",
+      xl: "1440px"
+    },
     extend: {},
   },
   plugins: [],
