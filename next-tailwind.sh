0a0d858056c0b68d78a4624a2b3cdf78f6c1c4a7
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-12-31 15:46:58 +0100

    Combine commits for next-tailwind initializer
    Include next folder in gitignore
    
    Update next init
    
    update loops
    
    Update next-tailwind initializer

--- next-tailwind.sh
+++ next-tailwind.sh
@@ -1,17 +1,47 @@
 set -e
 defaultExtension="--javascript"
+eslint="--no-eslint"
+appDir="--no-experimental-app"
+appFolder=false
+srcDir="--no-src-dir"
+srcFolder=''
+pkgMgr="--use-npm"
+appFolder=false
 
 value=''
-if [[ $1 == "ts" ]]
-then  
-defaultExtension='--typescript'
-fi
-npx create-next-app@latest . "$defaultExtension" --eslint
+for n in $(seq 1 $#); 
+do
+  if [[ $1 == "ts" ]]
+  then  
+  defaultExtension='--typescript'
+  elif [[ $1 == "lint" ]]
+  then  
+  eslint='--eslint'
+  elif [[ $1 == "app" ]]
+  then  
+  appDir='--experimental-app'
+  appFolder=true
+  elif [[ $1 == "src" ]]
+  then  
+  srcDir='--src-dir'
+  srcFolder="/src"
+  elif [[ $1 == "pnpm" ]]
+  then  
+  pkgMgr='--use-pnpm'
+  fi
+  shift
+done
+query="$defaultExtension $eslint $appDir $srcDir $pkgMgr --import-alias @/*"
+echo "Installing NextJs..."
+npx create-next-app@latest . $query
+echo "Installing TailwiindCSS..."
 npm install -D tailwindcss postcss autoprefixer
 npx tailwindcss init -p
-tailwindConfig='/** @type {import('tailwindcss').Config} */
+tailwindConfig='/** @type {import(tailwindcss).Config} */
 module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
@@ -19,14 +49,14 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}'
+};
+'
 echo "$tailwindConfig" > tailwind.config.js
 
 tailwindInit="@tailwind base;
 @tailwind components;
 @tailwind utilities;"
-echo "$tailwindInit" > ./styles/globals.css
-rm ./styles/Home.module.css
+
 
 index='import Head from "next/head";
 
@@ -44,9 +74,22 @@ export default function Home() {
   );
 }
 '
-echo "$index" > ./pages/index.*
+appindex='export default function Home() {
+  return <div>Hello World</div>;
+}
+'
+if [[ "$appFolder" = true ]]
+then
+echo "$tailwindInit" > ."$srcFolder"/app/globals.css
+rm ."$srcFolder"/app/page.module.css
+echo "$appindex" > ."$srcFolder"/app/page.*
+else
+echo "$index" > ."$srcFolder"/pages/index.*
+echo "$tailwindInit" > ."$srcFolder"/styles/globals.css
+rm ."$srcFolder"/styles/Home.module.css
+fi
 
-
+echo "Installing prettier plugin for Tailwind..."
 npm install -D prettier prettier-plugin-tailwindcss
 prettierConfig="module.exports = {
   plugins: [require('prettier-plugin-tailwindcss')],
@@ -57,6 +100,7 @@ echo "$prettierConfig" > prettier.config.js
 #Runs if git is installed on the hardware
 git init
 echo 'node_modules' > .gitignore
+echo '.next' >> .gitignore
 git add .
 git commit -m "Initial commit"
 
