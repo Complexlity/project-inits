abecd3b0d2896be56d117bd9e99106d4977a9e11
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-04-15 17:18:37 +0100

    clean code

--- react-vite-tailwind.sh
+++ react-vite-tailwind.sh
@@ -1,20 +1,43 @@
-set -e
-defaultTemplate="react"
-typescript=""
 
-value=''
-if [[ $1 == "ts" ]]
-then  
-value="-$1"
-typescript=" With TypeScript"
+#!/bin/bash
+
+set -euo pipefail # enable strict mode
+
+# Initialize variables
+default_template="react"
+typescript=""
+tailwind_init="@tailwind base;\n@tailwind components;\n@tailwind utilities;\n"
+app_code='
+function App() {
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default App;'
+prettier_config='module.exports = {
+  plugins: [require("prettier-plugin-tailwindcss")],
+};'
+
+# Parse command line arguments
+if [[ "${1:-}" == "ts" ]]; then
+  typescript=" with TypeScript"
+  default_template+="-$1"
 fi
-defaultTemplate+="$value"
-echo "Installing React$typescript..."
-npm create vite@latest . -- --template "$defaultTemplate"
+
+# Install and configure Vite with React template
+echo "Installing React${typescript}..."
+npm create vite@latest . -- --template "$default_template"
+
+# Install and configure TailwindCSS
 echo "Installing TailwindCSS..."
 npm install -D tailwindcss postcss autoprefixer
 npx tailwindcss init -p
-tailwindConfig='/** @type {import('tailwindcss').Config} */
+
+# Create TailwindCSS configuration file
+tailwind_config='/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: [
     "./index.html",
@@ -24,41 +47,29 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}'
-echo "$tailwindConfig" > tailwind.config.cjs
+};'
+echo "$tailwind_config" > tailwind.config.js
+echo "$tailwind_config" > tailwind.config.cjs
 
-tailwindInit="@tailwind base;
-@tailwind components;
-@tailwind utilities;"
-echo "$tailwindInit" > ./src/index.css
-
+# Create TailwindCSS CSS file
+echo -e "$tailwind_init" > ./src/index.css
 rm src/App.css
 
-app='
-function App() {
-  return (
-    <div className="App">
-    Hello World
-    </div>
-  )
-}
+# Create React app file
+echo "$app_code" > src/App.jsx
 
-export default App'
-echo "$app" > src/App.*
-
+# Install and configure Prettier with TailwindCSS plugin
 echo "Installing Prettier for Tailwind..."
 npm install -D prettier prettier-plugin-tailwindcss
-prettierConfig="module.exports = {
-  plugins: [require('prettier-plugin-tailwindcss')],
-}"
-echo "$prettierConfig" > prettier.config.cjs
+echo "$prettier_config" > prettier.config.cjs
 
+# Initialize Git repository (if installed)
+if command -v git >/dev/null 2>&1; then
+  git init
+  echo 'node_modules' > .gitignore
+  git add .
+  git commit -m "Initial commit"
+fi
 
-#Runs if git is installed on the hardware
-git init
-echo 'node_modules' > .gitignore
-git add .
-git commit -m "Initial commit"
-
+# Open project in default code editor
 code .
-
